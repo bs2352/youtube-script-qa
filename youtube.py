57a664cd201a0c348655890810f0ac675b00b1cf
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2023-08-02 23:04:14 +0900

    bug fix & refactoring

--- youtube.py
+++ youtube.py
@@ -40,18 +40,6 @@ if len(sys.argv) > 1:
     video_id = sys.argv[1]
 
 
-def _overlap_chunk (overlaps: deque[YoutubeScriptType]) -> ChunkModel|None:
-    if len(overlaps) == 0:
-        return None
-    new_chunk: ChunkModel = ChunkModel(id="", text="", start=0.0, duration=0.0, overlap=0)
-    for s in overlaps:
-        new_chunk.text += s['text']
-        new_chunk.duration += s['duration']
-        if new_chunk.start == 0.0:
-            new_chunk.start = s['start']
-    return new_chunk
-
-
 def create_index (video_id: str) -> GPTVectorStoreIndex:
     # YoutubeTranscriptReader = download_loader("YoutubeTranscriptReader")
     # loader = YoutubeTranscriptReader()
@@ -62,6 +50,18 @@ def create_index (video_id: str) -> GPTVectorStoreIndex:
     # documents = loader.load_data(ytlinks=["https://www.youtube.com/watch?v=XJRoDEctAwA"])
     # for doc in documents:
     #     print(doc.text, '-------------------')
+
+    # 後々開始時刻も利用したいのでチャンク分割を自作する
+    def _overlap_chunk (overlaps: deque[YoutubeScriptType]) -> ChunkModel|None:
+        if len(overlaps) == 0:
+            return None
+        new_chunk: ChunkModel = ChunkModel(id="", text="", start=0.0, duration=0.0, overlap=0)
+        for s in overlaps:
+            new_chunk.text += s['text']
+            new_chunk.duration += s['duration']
+            if new_chunk.start == 0.0:
+                new_chunk.start = s['start']
+        return new_chunk
 
     MAXLENGTH = 300
     OVERLAP_LENGTH = 3
@@ -103,40 +103,52 @@ def create_index (video_id: str) -> GPTVectorStoreIndex:
         else:
             overlaps.popleft()
             overlaps.append(script)
+    if chunk is not None:
+        chunks.append(chunk)
 
     # for chunk in chunks:
     #     print(chunk)
     # sys.exit(0)
 
-    documents = [Document(text=chunk.text.replace("\n", " "), doc_id=chunk.id) for chunk in chunks]
-    # print(documents)
-    # documents = documents[0:2]
+    documents = [
+        Document(text=chunk.text.replace("\n", " "), doc_id=chunk.id) for chunk in chunks
+    ]
 
     index: GPTVectorStoreIndex = GPTVectorStoreIndex.from_documents(documents)
 
     return index
 
 
+# インデックス作成
 index_dir = f'{INDEX_STORAGE_DIR}/{video_id}'
 if not os.path.isdir(index_dir):
+    print("creating index ...", end="", flush=True)
     index: GPTVectorStoreIndex = create_index(video_id)
+    print("fin", flush=True)
+    print(f'save index to {index_dir} ...', end="", flush=True)
     os.mkdir(index_dir)
     index.storage_context.persist(persist_dir=index_dir)
+    print("fin", flush=True)
 else:
     from llama_index import StorageContext, load_index_from_storage
+    print(f'load index from {index_dir} ...', end="", flush=True)
     storage_context: StorageContext = StorageContext.from_defaults(persist_dir=index_dir)
     index: GPTVectorStoreIndex = load_index_from_storage(storage_context) # type: ignore
+    print("fin", flush=True)
 
 
 # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, force=True)
-query_engine = index.as_query_engine()
 
+# より詳細にクエリエンジンを制御したい場合は以下を参照
+# https://gpt-index.readthedocs.io/en/v0.6.26/guides/primer/usage_pattern.html
+query_engine = index.as_query_engine(similarity_top_k=3)
 while True:
     query = input("Query: ").strip()
     if query == "":
         break
+    print('Answer: ', end="", flush=True)
     response = query_engine.query(query)
-    print("Answer: ", response)
+    print(f'{str(response).strip()}\n')
     for node in response.source_nodes:
         id = ""
         for key, val in node.node.dict()["relationships"].items():
