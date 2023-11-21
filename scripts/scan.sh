169f9d7dedbd79207afb1160ba0e338d102d0328
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2023-11-21 21:02:21 +0900

    テスト用スクリプトを追加

---
+++ scripts/scan.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+CUR_DIR=`pwd`
+if [ `basename $CUR_DIR` != "scripts" ]; then
+    cd scripts
+fi
+
+VID_LIST="../vid.txt" # 動画IDのリスト
+
+for vid in `cat $VID_LIST`
+do
+    cur_dir=`pwd`
+    cd ..
+    echo "# ${vid}"
+
+    # 任意の処理を書く
+    python sample.py $vid x
+    python sample.py $vid
+
+    cd $cur_dir
+    read -p "Press enter to continue" input
+done
