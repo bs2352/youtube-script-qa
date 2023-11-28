002da2107f5d70f90a886077ef709e81217a8fa7
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2023-11-28 21:45:22 +0900

    openai v1.x.x でのプロキシ指定方法の変更への対応

--- yts/__init__.py
+++ yts/__init__.py
@@ -2,4 +2,5 @@ import openai
 import os
 
 if "https_proxy" in os.environ:
-    openai.proxy = os.environ["https_proxy"]
+    # openai.proxy = os.environ["https_proxy"]
+    os.environ["OPENAI_PROXY"] = os.environ["https_proxy"]
