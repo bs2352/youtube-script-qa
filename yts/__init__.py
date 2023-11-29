647279d888adbe0f6aed53308972d9d56c3d2a0a
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2023-11-29 20:49:04 +0900

    不要コードの削除

--- yts/__init__.py
+++ yts/__init__.py
@@ -1,9 +1,7 @@
-import openai
 import os
 import dotenv
 
 dotenv.load_dotenv()
 
 if "https_proxy" in os.environ:
-    # openai.proxy = os.environ["https_proxy"]
     os.environ["OPENAI_PROXY"] = os.environ["https_proxy"]
