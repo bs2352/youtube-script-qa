64d8d3823f21a3f1bcc542ff15cea88c3f9b74d7
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2023-12-13 18:13:05 +0900

    .envから環境変数を読み取るように修正

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -5,15 +5,20 @@ if [ `basename $CUR_DIR` != "scripts" ]; then
     cd scripts
 fi
 
+CHECK=`which python | grep .venv`
+if [ "x${CHECK}" = "x" ]; then
+    source ../.venv/bin/activate
+fi
 
+source ../.env
+export SUMMARY_STORE_DIR="../${SUMMARY_STORE_DIR}" # 要約の保存ディレクトリ
 VID_LIST="../vid.txt" # 動画IDのリスト
-SUMMARY_DIR="../data/summaries" # 要約の保存先
 
 
 main() {
     case $1 in
-        "-s" )        summarize ;;
-        "summarize" ) summarize ;;
+        "-s" )        summarize $2;;
+        "summarize" ) summarize $2;;
         "summary" )   summary $2;;
         "list" )      list ;;
         * )           list;;
@@ -25,6 +30,9 @@ main() {
 summarize() {
     for vid in `cat $VID_LIST`
     do
+        if [ "x${1}" != "x" -a "x${1}" != "x${vid}" ]; then
+            continue
+        fi
         cur_dir=`pwd`
         cd ..
         echo "# ${vid}"
@@ -38,9 +46,9 @@ summarize() {
 
 # 要約ディレクトリにある全ての要約を一覧表示する
 list() {
-    for vid in `ls $SUMMARY_DIR`
+    for vid in `ls $SUMMARY_STORE_DIR`
     do
-        summary_file="$SUMMARY_DIR/$vid"
+        summary_file="$SUMMARY_STORE_DIR/$vid"
         title=`cat $summary_file | jq | grep title | head -n 1 | cut -d ':' -f2 | sed -e 's/,$//' | sed -e 's/^[ \t]*//' | sed -e 's/^"//' | sed -e 's/"$//'`
         author=`cat $summary_file | jq | grep author | cut -d ':' -f2 | sed -e 's/,$//' | sed -e 's/^[ \t]*//' | sed -e 's/^"//' | sed -e 's/"$//'`
         echo -e "$vid\t$author\t$title"
@@ -49,13 +57,13 @@ list() {
 
 # 要約ディレクトリの要約を順番に表示する
 summary() {
-    for vid in `ls $SUMMARY_DIR`
+    for vid in `ls $SUMMARY_STORE_DIR`
     do
         if [ "x${1}" != "x" -a "x${1}" != "x${vid}" ]; then
             continue
         fi
         echo $vid
-        # summary_file="$SUMMARY_DIR/$vid"
+        # summary_file="$SUMMARY_STORE_DIR/$vid"
         # cat $summary_file | jq | less
         cur_dir=`pwd`
         cd ..
