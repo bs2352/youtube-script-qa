b3a7da211d59df5be246cb4b58d8bb57eaad8f06
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2023-12-13 12:15:39 +0900

    bug fix

--- yts/__init__.py
+++ yts/__init__.py
@@ -1,7 +1,7 @@
 import os
 import dotenv
 
-dotenv.load_dotenv(override=False)
+dotenv.load_dotenv(override=True)
 
 if "https_proxy" in os.environ:
     os.environ["OPENAI_PROXY"] = os.environ["https_proxy"]
