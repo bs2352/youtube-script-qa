d1a106218eb6f9934e604af0e0d1f8289928bd10
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2023-12-17 15:13:07 +0900

    vscodeエラー解消＋Youtube埋め込み

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import YouTube from 'react-youtube'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
       <div>
       </div>
       <h1>Youtube Supporter</h1>
+      <YouTube videoId="cEynsEWpXdA" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
