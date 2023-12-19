3e5a225d31354473451899a08bff76db14097f4f
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2023-12-19 20:48:25 +0900

    APIサーバーにQAと字幕取得のAPIを追加

--- api.py
+++ api.py
@@ -1,18 +1,23 @@
-from typing import Optional
+from typing import Optional, List
 import logging
 import os
 from contextlib import asynccontextmanager
+from youtube_transcript_api import YouTubeTranscriptApi
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 from yts.summarize import YoutubeSummarize
-from yts.types import SummaryResultModel
-import os
+from yts.qa import YoutubeQA
+from yts.types import SummaryResultModel, YoutubeTranscriptType
 
 
 STATIC_FILES_DIR = "frontend/dist"
+DEFAULT_VIDEO_ID = "cEynsEWpXdA"
+DEFAULT_QUESTION = "ファインチューニングをするとなぜ精度が落ちるのですか？"
+DEFAULT_REF_SOURCES = 3
+DEFAULT_TRANSCRIPT_LANGUAGES = ["ja", "en", "en-US"]
 
 
 # 起動時と停止時の処理はここに書く
@@ -31,7 +36,39 @@ app = FastAPI(lifespan=lifespan)
 
 
 class SummaryRequestModel (BaseModel):
+    vid: str = DEFAULT_VIDEO_ID
+
+class SummaruResponseModel (BaseModel):
     vid: str
+    summary: SummaryResultModel
+
+class QARequestModel (BaseModel):
+    vid: str = DEFAULT_VIDEO_ID
+    question: str = DEFAULT_QUESTION
+    ref_sources: int = DEFAULT_REF_SOURCES
+
+class SourceModel (BaseModel):
+    score: float
+    time: str
+    source: str
+
+class QAResponseModel (BaseModel):
+    vid: str
+    question: str
+    answer: str
+    sources: List[SourceModel]
+
+class TranscriptRequestModel (BaseModel):
+    vid: str = DEFAULT_VIDEO_ID
+
+class TranscriptModel (BaseModel):
+    text: str
+    start: float
+    duration: float
+
+class TranscriptResponseModel (BaseModel):
+    vid: str
+    transcripts: List[TranscriptModel]
 
 
 @app.post (
@@ -49,5 +86,57 @@ async def summary (request_body: SummaryRequestModel):
     except Exception as e:
         logging.error(f"[{vid}] {str(e)}", exc_info=True)
         return {"vid": request_body.vid, "error": str(e)}, 500
-    return summary
+    return SummaruResponseModel(vid=vid, summary=summary)
 
+
+@app.post (
+    "/qa",
+    summary="QA about Youtube video content",
+    description="Please specify video ID (such as cEynsEWpXdA, nYx5UaKI8mE) and question.",
+    tags=["QA"]
+)
+async def qa (request_body: QARequestModel):
+    vid: str = request_body.vid
+    question: str = request_body.question
+    ref_sources: int = request_body.ref_sources
+    sources: List[SourceModel] = []
+    try:
+        yqa: YoutubeQA = YoutubeQA(vid, ref_sources, True, False, False)
+        answer: str = await yqa.arun(question)
+        for score, _, time, source in yqa.get_source():
+            sources.append(SourceModel(score=score, time=time, source=source))
+    except Exception as e:
+        logging.error(f"[{vid}] {str(e)}", exc_info=True)
+        return {"vid": request_body.vid, "error": str(e)}, 500
+    return QAResponseModel(
+        vid=vid, question=question, answer=answer, sources=sources
+    )
+
+
+@app.post (
+    "/transcript",
+    summary="get transcript",
+    description="get transcript",
+    tags=["Transcript"]
+)
+async def transcript (request_body: TranscriptRequestModel):
+    vid: str = request_body.vid
+    try:
+        transcripts: List[YoutubeTranscriptType] = YouTubeTranscriptApi.get_transcript(vid, languages=DEFAULT_TRANSCRIPT_LANGUAGES)
+    except Exception as e:
+        logging.error(f"[{vid}] {str(e)}", exc_info=True)
+        return {"vid": request_body.vid, "error": str(e)}, 500
+    return TranscriptResponseModel(
+        vid=vid,
+        transcripts=[
+            TranscriptModel(
+                text=transcript["text"],
+                start=transcript["start"],
+                duration=transcript["duration"]
+            ) for transcript in transcripts
+        ]
+    )
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8080, loop='asyncio', reload=True)
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,8 +4,14 @@ pythonpath = './'
 # ワーカー数
 workers = 2
 
-# ワーカーのクラス、*2 にあるようにUvicornWorkerを指定 (Uvicornがインストールされている必要がある)
-worker_class = 'uvicorn.workers.UvicornWorker'
+# ワーカーのクラスUvicornWorkerを指定 (Uvicornがインストールされている必要がある)
+# uvicorn.run(loop='asyncio')を指定するために独自クラスを作成する
+# ref. https://www.uvicorn.org/deployment/#gunicorn
+from uvicorn.workers import UvicornWorker
+class MyUvicornWorker(UvicornWorker):
+    CONFIG_KWARGS = {"loop": "asyncio"}
+# worker_class = 'uvicorn.workers.UvicornWorker'
+worker_class = 'gunicorn_config.MyUvicornWorker'
 
 # IPアドレスとポート
 bind = '127.0.0.1:8080'
@@ -23,7 +29,7 @@ pidfile = 'app.pid'
 # errorlog = './logs/error_log.txt'
 
 # プロセスの名前
-# proc_name = 'my_python_app'
+proc_name = 'yts_app'
 
 # アクセスログ
 # accesslog = './logs/access_log.txt'
