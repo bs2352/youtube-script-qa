da88d59765b8592b06f8279c98511dce7e7ecd10
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2024-01-21 11:45:03 +0900

    起動スクリプトの修正

--- start.sh
+++ start.sh
@@ -2,16 +2,47 @@
 
 VENV_BIN=".venv/bin/"
 
-cd frontend
-if [ ! -d "node_modules" ];
-then
-    npm install
-fi
-if [ ! -d "dist" ];
-then
-    npm run build
-fi
 
-# run backend server
-cd ..
-$VENV_BIN/gunicorn -c gunicorn_config.py restapi:app
+main() {
+    case $1 in
+        "-f" )        run_frontend;;
+        "-i" )        remove_data;;
+        * )           run_backend;;
+    esac
+}
+
+run_frontend () {
+    prepare_frontend
+    cd frontend
+    npm run dev
+}
+
+run_backend () {
+    prepare_frontend
+    build_frontend
+    $VENV_BIN/gunicorn -c gunicorn_config.py restapi:app
+}
+
+prepare_frontend() {
+    cd frontend
+    if [ ! -d "node_modules" ];
+    then
+        npm install
+    fi
+    cd ..
+}
+
+build_frontend () {
+    cd frontend
+    if [ ! -d "dist" ];
+    then
+        npm run build
+    fi
+    cd ..
+}
+
+remove_data () {
+    rm -rf data
+}
+
+main $1
