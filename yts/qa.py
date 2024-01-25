63ba65cf5a1a79a982510300c7585deffdd45859
bs2352 <nakaichi@LAPTOP-U17K9DRF>
2024-01-25 19:39:29 +0900

    QA・検索時のチャンクサイズを変更

--- yts/qa.py
+++ yts/qa.py
@@ -184,8 +184,8 @@ class YoutubeQA:
 
         self._debug("creating index ...", end="", flush=True)
 
-        MAXLENGTH = 300
-        OVERLAP_LENGTH = 3
+        MAXLENGTH = 500
+        OVERLAP_LENGTH = 5
         transcriptions: List[YoutubeTranscriptType] = YouTubeTranscriptApi.get_transcript(video_id=self.vid, languages=["ja", "en", "en-US"])
         chunks: List[TranscriptChunkModel] = divide_transcriptions_into_chunks(
             transcriptions,
